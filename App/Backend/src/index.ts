c16435fbd331346373be458518183aac19bdbc07
DanielCogiel <dcogiel.work@gmail.com>
2023-04-17 14:32:02 +0200

    Dodanie komponentu strony z zapisanymi kursami

--- App/Backend/src/index.ts
+++ App/Backend/src/index.ts
@@ -2,6 +2,9 @@ import express, {Express, Request, Response} from 'express'
 import CourseModel from './interfaces/CourseModel';
 import cors from 'cors';
 
+const API_URL: string = '/api'
+const COURSES_URL: string = '/courses'
+
 const courses: CourseModel[] = [
     {
         id: 1,
@@ -22,20 +25,20 @@ const courses: CourseModel[] = [
     },
     {
         id: 2,
-        title: "Python course for beginners",
-        language: "PL",
+        title: "Python course for intermediates",
+        language: "EN",
         date: {
-            start: "01.12.2023",
-            finish: "31.12.2023"
+            start: "05.05.2023",
+            finish: "30.06.2023"
         },
         hours: {
             start: "8:00",
-            finish: "16:00",
-            times: 8
+            finish: "18:00",
+            times: 4
         },
-        level: "Basic",
+        level: "Intermediate",
         location: "Remote",
-        trainer: "Jan Kowalski"
+        trainer: "Jan Nowak"
     },
     {
         id: 3,
@@ -56,6 +59,26 @@ const courses: CourseModel[] = [
     },
     {
         id: 4,
+        title: "Python course for intermediates",
+        language: "EN",
+        date: {
+            start: "05.05.2023",
+            finish: "30.06.2023"
+        },
+        hours: {
+            start: "8:00",
+            finish: "18:00",
+            times: 4
+        },
+        level: "Intermediate",
+        location: "Remote",
+        trainer: "Jan Nowak"
+    }
+]
+
+const personalCourses: CourseModel[] = [
+    {
+        id: 1,
         title: "Python course for beginners",
         language: "PL",
         date: {
@@ -70,6 +93,23 @@ const courses: CourseModel[] = [
         level: "Basic",
         location: "Remote",
         trainer: "Jan Kowalski"
+    },
+    {
+        id: 2,
+        title: "Python course for intermediates",
+        language: "EN",
+        date: {
+            start: "05.05.2023",
+            finish: "30.06.2023"
+        },
+        hours: {
+            start: "8:00",
+            finish: "18:00",
+            times: 4
+        },
+        level: "Intermediate",
+        location: "Remote",
+        trainer: "Jan Nowak"
     }
 ]
 
@@ -78,8 +118,12 @@ const PORT: number = 4000;
 
 app.use(cors());
 
-app.get('/api', (req: Request, res: Response) => {
+app.get(`${API_URL}${COURSES_URL}/all`, (req: Request, res: Response) => {
     res.json(courses);
+})
+
+app.get(`${API_URL}${COURSES_URL}/personal`, (req: Request, res: Response) => {
+    res.json(personalCourses);
 })
 
 app.listen(PORT, () => {
