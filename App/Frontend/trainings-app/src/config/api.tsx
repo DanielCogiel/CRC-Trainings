80f6888adf02100254d07c10dbc839b1fefccf4a
DanielCogiel <dcogiel.work@gmail.com>
2023-04-27 13:35:18 +0200

    Dodanie działającego endpointu rejestracji użytkownika i obsługa na froncie

--- App/Frontend/trainings-app/src/config/api.tsx
+++ App/Frontend/trainings-app/src/config/api.tsx
@@ -1,5 +1,7 @@
 const SERVER_URL = 'http://localhost:4000'
 const API_URL = `${SERVER_URL}/api`;
+const REGISTER_URL = `${API_URL}/register`;
+const LOGIN_URL = `${API_URL}/login`
 const COURSES_API_URL = `${API_URL}/courses`
 const REGISTER_COURSE_URL = `${COURSES_API_URL}/register`
 const ALL_COURSES_URL = `${COURSES_API_URL}/all`
@@ -10,5 +12,9 @@ export const API_URLS = {
         ALL: ALL_COURSES_URL,
         PERSONAL: PERSONAL_COURSES_URL,
         REGISTER: REGISTER_COURSE_URL
+    },
+    AUTH: {
+        REGISTER: REGISTER_URL,
+        LOGIN: LOGIN_URL
     }
 }
